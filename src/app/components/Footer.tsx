bab3493963fc9335c0d53feeacace765c08c2c43
MaryamKhan2001 <maryamkhanhomm@gmail.com>
2024-11-01 21:11:29 +0500

    first commit

---
+++ src/app/components/Footer.tsx
@@ -0,0 +1,20 @@
+export default function Footer() {
+    return (
+        <div>
+        <footer>
+        
+            <h2>Contact Us</h2>
+            <p>Phone: 987654367</p>
+            <h2>Email</h2>
+            <p>lilyhom@gmail.com</p>
+            <h2>Visit Us At</h2>
+            <p>XYZ ROAD ,LILY STREET HOUSTON</p>
+            <hr />
+            <div className="flex items-center">
+            <h2>VISIT OUR BRANCH FOR FURTHER DETAILS</h2>
+            </div>
+            </footer>
+            </div>
+        
+    );
+} 
